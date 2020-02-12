133411652ad41106a0e14c68be570f60952b2627
keisuke ohashi <kog71903@gmail.com>
2020-02-13 00:49:37 +0900

    ユーザーがゴキブリを登録できるように（user.gokisのアソシエーションはフォームからはまだできない）

--- app/models/goki.rb
+++ app/models/goki.rb
@@ -1,10 +1,12 @@
 class Goki < ApplicationRecord
 	belongs_to :genre
 	has_many :move
-	has_many :goki_user, foreign_key: "goki_id", dependent: :destroy
-	has_many :user, through: :goki_user
+	has_many :goki_users, dependent: :destroy
+	has_many :users, through: :goki_users
 
 	validates :name, :img, presence: {message: 'must not be empty.'}
 	validates :description, length: {maximum: 35, message: "must be under 35 words"}
 	validates :genre_id, numericality: {only_integer:true, less_than:4, greater_than:0}
+
+	GOKI_SELECTION = ["bossgoki", "facegoki", "goki", "mildgoki", "turkishgoki", "coolgoki", "germangoki", "greengoki", "minigoki", "stripegoki", "whitegoki"]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 	devise :database_authenticatable, :registerable,
 			 :recoverable, :rememberable, :validatable
-	
-	has_many :goki_user, foreign_key: "user_id", dependent: :destroy
-	has_many :goki, through: :goki_user
+
+	has_many :goki_users, dependent: :destroy
+	has_many :gokis, through: :goki_users
 end
